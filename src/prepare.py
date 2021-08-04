3a1f8d824ab846098632b63618df7dc371a1241f
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-08-04 17:52:35 +0300

    feat(data): Add CreateUsdaDataset class
    Class for creating main dataset from USDA API data

--- src/prepare.py
+++ src/prepare.py
@@ -1,52 +1,121 @@
-import io
-import os
-import random
-import re
-import sys
-import xml.etree.ElementTree
+"""
+Data prepare module from USDA.
 
+API methods: https://apps.fas.usda.gov/opendataweb/home
+"""
+import json
+import pandas
+import requests
 import yaml
 
-
-params = yaml.safe_load(open("params.yaml"))["prepare"]
-
-if len(sys.argv) != 2:
-    sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython prepare.py data-file\n")
-    sys.exit(1)
-
-# Test data set split ratio
-split = params["split"]
-random.seed(params["seed"])
-
-input = sys.argv[1]
-output_train = os.path.join("data", "prepared", "train.tsv")
-output_test = os.path.join("data", "prepared", "test.tsv")
+from auth import auth
 
 
-def process_posts(fd_in, fd_out_train, fd_out_test, target_tag):
-    num = 1
-    for line in fd_in:
+def main():
+    """
+    Data prepare pipeline.
+    """
+    dataframe = CreateUsdaDataset()
+    dataframe.save_dataframe()
+
+
+class CreateUsdaDataset:
+
+    def __init__(self):
+        self.url = 'https://apps.fas.usda.gov/OpenData'
+        self.headers_usda = {
+            'Accept': 'application/json',
+            'API_KEY': auth.usda_apikey
+        }
+
+    def save_dataframe(self):
+        """
+        Save dataframe with all commodities in project data dir.
+        """
+        dataframe = self.merge_dataset_and_catalogs()
         try:
-            fd_out = fd_out_train if random.random() > split else fd_out_test
-            attr = xml.etree.ElementTree.fromstring(line).attrib
+            dataframe.to_parquet('data/data.parquet')
+            print('All data saved...')
+        except FileNotFoundError as e:
+            print(e)
 
-            pid = attr.get("Id", "")
-            label = 1 if target_tag in attr.get("Tags", "") else 0
-            title = re.sub(r"\s+", " ", attr.get("Title", "")).strip()
-            body = re.sub(r"\s+", " ", attr.get("Body", "")).strip()
-            text = title + " " + body
+    def merge_dataset_and_catalogs(self) -> pandas.DataFrame:
+        """
+        Merge main dataset an catalogs with description of measures.
+        :return: merged dataframe
+        """
+        dataframe = self.append_commodities_into_dataset()
+        print('Get catalogs through API...')
+        commodities = self._get_api_data_by_link(f'{self.url}/api/psd/commodities')
+        print(commodities.head())
+        attributes = self._get_api_data_by_link(f'{self.url}/api/psd/commodityAttributes')
+        print(attributes.head())
+        units = self._get_api_data_by_link(f'{self.url}/api/psd/unitsOfMeasure')
+        print(units.head())
+        print('Merge main dataset and catalogs with features key...')
+        dataframe = dataframe.merge(
+            commodities, how='left', on='commodityCode'
+        ).merge(
+            attributes, how='left', on='attributeId'
+        ).merge(
+            units, how='left', on='unitId'
+        )
+        print(dataframe.head())
+        print('About merged dataframe:')
+        print(dataframe.info())
+        return dataframe
 
-            fd_out.write("{}\t{}\t{}\n".format(pid, label, text))
+    def append_commodities_into_dataset(self) -> pandas.DataFrame:
+        """
+        Append commodity data into common dataframe.
+        :return: dataframe with commodities data
+        """
+        dataframe = pandas.DataFrame()
+        print('Get all possible commodity codes...')
+        codes = self._get_api_data_by_link(f'{self.url}/api/psd/commodities')
+        commodity_codes = list(codes['commodityCode'])
+        # Reporting period
+        period_start = yaml.safe_load(open('params.yaml'))['period']['start']
+        period_stop = yaml.safe_load(open('params.yaml'))['period']['stop']
+        print(f'Reporting period from: {period_start} to: {period_stop} ')
 
-            num += 1
-        except Exception as ex:
-            sys.stderr.write(f"Skipping the broken line {num}: {ex}\n")
+        print('Iteration by commodity codes and periods...')
+        for code in commodity_codes:
+            for year in range(period_start, period_stop, 1):
+                tmp_data = self.get_data_by_commodity_and_year(code, year)
+                dataframe = dataframe.append(tmp_data)
+        print(dataframe.head())
+        print('About appended dataframe:')
+        print(dataframe.info())
+        return dataframe
+
+    def get_data_by_commodity_and_year(self, commodity_code: str,
+                                       year: int) -> pandas.DataFrame:
+        """
+        Get data by commodity code.
+        :return: dataframe with commodity data
+        """
+        address = f'/api/psd/commodity/{commodity_code}' \
+                  f'/country/all/year/{year}'
+        request = requests.get(f'{self.url}{address}',
+                               headers=self.headers_usda)
+        response = json.loads(request.text)
+        commodity = pandas.DataFrame(response)
+        return commodity
+
+    def _get_api_data_by_link(self, link: str) -> pandas.DataFrame:
+        """
+        Import data from usda API.
+        :return: dataframe with data
+        """
+        try:
+            request = requests.get(link, headers=self.headers_usda)
+            response = json.loads(request.text)
+            data = pandas.DataFrame(response)
+            return data
+        except json.decoder.JSONDecodeError as e:
+            print(e)
 
 
-os.makedirs(os.path.join("data", "prepared"), exist_ok=True)
-
-with io.open(input, encoding="utf8") as fd_in:
-    with io.open(output_train, "w", encoding="utf8") as fd_out_train:
-        with io.open(output_test, "w", encoding="utf8") as fd_out_test:
-            process_posts(fd_in, fd_out_train, fd_out_test, "<python>")
+if __name__ == '__main__':
+    main()
